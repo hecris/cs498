4c9348e0cb3d990a509b921e5d6840bc9d41f0e5
hecris <christopher.he33@myhunter.cuny.edu>
2023-09-07 20:53:13 -0400

    complete viz

--- viz/viz.py
+++ viz/viz.py
@@ -1,4 +1,29 @@
-# Simple pygame program
+import numpy as np
+import random
+
+def is_derangement(arr):
+    return not any (i==x for i, x in enumerate(arr))
+
+def generate_derangement(n):
+    a = list(np.random.permutation(n))
+    while not is_derangement(a):
+        a = list(np.random.permutation(n))
+    return a
+
+def generate_swap(arr):
+    bag = [i for i, x in enumerate(arr) if x != i]
+    if not bag:
+        return -1, -1
+    i = random.choice(bag)
+    j = random.choice(bag)
+
+    while (i == j):
+        i = random.choice(bag)
+        j = random.choice(bag)
+
+    return i, j
+
+
 
 # Import and initialize the pygame library
 import pygame
@@ -9,11 +34,15 @@ pygame.init()
 screen = pygame.display.set_mode([500, 500])
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 
 # Run until the user asks to quit
 running = True
-arr = [1,2,3]
+N = 10
+arr = generate_derangement(N)
 FONT = pygame.font.SysFont('Arial', 25)
+swaps = 0
 
 while running:
 
@@ -25,19 +54,38 @@ while running:
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    pos = 10
+    start = 10
+    pos = start
     BOX_SIZE = 30
     # Draw a solid blue circle in the center
-    for x in arr:
-        rect_obj = pygame.draw.rect(screen, (BLACK), (pos, 250, BOX_SIZE, BOX_SIZE), 2)
+    rect_objs = []
+    for i, x in enumerate(arr):
+        color = BLUE if i == x else BLACK
+        rect_obj = pygame.draw.rect(screen, (color), (pos, 250, BOX_SIZE, BOX_SIZE), 2)
+        rect_objs.append(rect_obj)
         text_surface_object = FONT.render(str(x), True, BLACK)
         text_rect = text_surface_object.get_rect(center=rect_obj.center)
         screen.blit(text_surface_object, text_rect)
-        pos += BOX_SIZE + 1
+        pos += BOX_SIZE
+
+    i, j = generate_swap(arr)
+    if i != -1:
+        rect_obj = pygame.draw.rect(screen, (RED), (start + BOX_SIZE * i, 250, BOX_SIZE, BOX_SIZE), 2)
+        rect_obj = pygame.draw.rect(screen, (RED), (start + BOX_SIZE * j, 250, BOX_SIZE, BOX_SIZE), 2)
+        arr[i], arr[j] = arr[j], arr[i]
+        swaps += 1
+    else:
+        arr = generate_derangement(N)
+        pygame.time.wait(1000)
+        swaps = 0
+
+    screen.blit(FONT.render('swaps: {}'.format(swaps), True, BLACK), (0,0))
+
 
     # Flip the display
     pygame.display.flip()
-    time.sleep(1)
+    pygame.time.wait(1000)
+    # time.sleep(1)
 
 # Done! Time to quit.
 pygame.quit()
